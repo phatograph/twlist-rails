65b2f0f469e13f0ceb01b36affebdbfd1a755146
Phat Wangrungarun <phatograph@gmail.com>
2013-05-29 15:52:42 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweather::Application.config.secret_token = '626e8b93303721910c067ae4182e98e68cf974ce886450a34b9114b5816b9a9102fcbe2d32c356e45094207befdaf3a440a7375ea4904df4df05b477c9bb240c'
